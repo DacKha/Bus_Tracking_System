116458c154ddc285251c6549711151f2b1da3401
unknown <hodackha36@gmail.com>
2025-11-08 22:28:32 +0700

    Demo

--- font-end/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Navbar from "../component/Navbar";
-export default function HomePage() {
-  return (
-    <div className="flex flex-col h-screen w-screen bg-white">
-      <h1>Trang chủ</h1>
-    </div>
-  );
-}
